af06bafc26d8e719108b45374427777d90466c97
Ahmed Hammad <hammad97official@gmail.com>
2020-03-16 01:37:48 +0200

    Move configs to seperate file

---
+++ configService/src/main/java/com/hammadltd/configservice/interfaces/IConfigs.java
@@ -0,0 +1,8 @@
+package com.hammadltd.configservice.interfaces;
+
+public interface IConfigs {
+    String getConfig1();
+    String getConfig2();
+    String getConfig3();
+    String getConfig4();
+}
---
+++ configService/src/main/java/com/hammadltd/configservice/models/Configs.java
@@ -0,0 +1,49 @@
+package com.hammadltd.configservice.models;
+
+import com.hammadltd.configservice.interfaces.IConfigs;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "configs")
+@PropertySource("classpath:configs.properties")
+public class Configs implements IConfigs {
+    private String config1;
+    private String config2;
+    private String config3;
+    private String config4;
+
+    public String getConfig1() {
+        return config1;
+    }
+
+    public void setConfig1(String config1) {
+        this.config1 = config1;
+    }
+
+    public String getConfig2() {
+        return config2;
+    }
+
+    public void setConfig2(String config2) {
+        this.config2 = config2;
+    }
+
+    public String getConfig3() {
+        return config3;
+    }
+
+    public void setConfig3(String config3) {
+        this.config3 = config3;
+    }
+
+    public String getConfig4() {
+        return config4;
+    }
+
+    public void setConfig4(String config4) {
+        this.config4 = config4;
+    }
+
+}
