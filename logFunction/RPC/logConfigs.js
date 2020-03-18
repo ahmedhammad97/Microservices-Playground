7e69dd347e6e3da1ade98d7266f1fc665b93fbf0
Ahmed Hammad <hammad97official@gmail.com>
2020-03-18 19:17:03 +0200

    Add RPC client to lambda function

---
+++ logFunction/RPC/logConfigs.js
@@ -0,0 +1,27 @@
+const secrets = require("../secrets");
+const PROTO_PATH = "./configs.proto";
+const grpc = require("grpc");
+const protoLoader = require('@grpc/proto-loader');
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, { 
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+});
+
+function configsRPC(configs) {
+    var routeguide = grpc.loadPackageDefinition(packageDefinition).routeguide;
+    var creds = grpc.credentials.createInsecure();
+    var client = new routeguide.RouteGuide('localhost:7789', creds);
+
+    return new Promise((resolve, reject) => {
+        let call = client.GetConfigs(configs, secrets.rpcSecret);
+
+        call.on("data", data => { resolve(data); });
+
+        call.on("error", err => { reject(err); });
+    });
+}
+
+module.exports = configsRPC;
--- logFunction/handler.js
+++ logFunction/handler.js
@@ -1,27 +1,22 @@
 'use strict';
 const decryption = require("./decryption");
+const logConfigs = require("./RPC/logConfigs");
 
 module.exports.logSQSMsgs = async event => {
-  // Get configs
-  // Print configs
 
-  for (record of records) {
+  logConfigs(["config1", "config3", "config4"]).then(data => {
+    console.log(data);
+  }).catch (err => {
+    console.log(err);
+  });
+  
+  for (record of event.records) {
     let id = record.body.split(" ")[0];
     let encodedMsg = record.body.split(" ")[1];
     let log = {"id": id, "message": decryption(encodedMsg)}
     console.log(log);
   }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  return { statusCode: 200 };
 
 };
